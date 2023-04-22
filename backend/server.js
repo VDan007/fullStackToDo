78f4ed6da6a76e84b5396a27834943a70d635d20
DanielVarjaskéri <varjaskerid@gmail.com>
2023-04-23 00:15:52 +0200

    next is connect frontend

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,10 @@ app.listen(PORT,()=> console.log(  `Server running on port ${PORT}` ) );
 
 //get all todos
 app.get('/todos', async (req,res) => {
+    const userEmail = 'ania@test.com';
 
     try{
-      const todos = await pool.query('select * from todos');
+      const todos = await pool.query('select * from todos where user_email=$1',[userEmail]);
       res.json(todos.rows);
 
     }catch(err){
